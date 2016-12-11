83196b5f789bb9aca188a009c31e1954a865cd4f
ebrahimamini <aliamini5715@gmail.com>
2016-12-11 13:09:22 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,6 +1,6 @@
 <?php
 define('BOT_TOKEN', '**TOKEN**');
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
+define('API_URL', 'https://api.telegram.org/bot'.308392071:AAGqXRfdIZq8OCfPb367771Y4KCsKAzL4nE.'/');
 
 function apiRequestWebhook($method, $parameters) {
   if (!is_string($method)) {
@@ -115,7 +115,7 @@ function processMessage($message) {
   // process incoming message
   $boolean = file_get_contents('booleans.txt');
   $booleans= explode("\n",$boolean);
-  $admin = **ADMIN**;
+  $admin = **308392071**;
   $message_id = $message['message_id'];
   $rpto = $message['reply_to_message']['forward_from']['id'];
   $chat_id = $message['chat']['id'];
